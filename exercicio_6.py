a0e3821521bc73312de976fabe132c27195755bc
Náthaly Hayashi Komatsu <nahkomatsu@gmail.com>
2026-04-16 11:48:03 -0300

    Update exercicio_6.py

--- exercicio_6.py
+++ exercicio_6.py
@@ -52,3 +52,22 @@ Qual país você vai viajar? China
 Resposta:
 Não temos essa moeda em caixa.
 """
+pais = input("Qual país você vai viajar?")
+
+if pais == "Estados Unidos":
+    reais = float(input("Quantos reais você quer converter?"))
+    valor = reais / 5
+    print(f"{valor:.2f} USD")
+
+elif pais == "Argentina":
+    reais = float(input("Quantos reais você quer converter?"))
+    valor = reais * 180
+    print(f"{valor:.2f} ARS")
+
+elif pais == "Japão":
+    reais = float(input("Quantos reais você quer converter?"))
+    valor = reais * 30
+    print(f"{valor:.2f} JPY")
+
+else:
+    print("Não temos essa moeda em caixa.")
