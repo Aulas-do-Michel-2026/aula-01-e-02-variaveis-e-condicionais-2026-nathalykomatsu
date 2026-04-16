a28fda182ebed40475e5d8b0b6ba6c596ca4bd3f
Náthaly Hayashi Komatsu <nahkomatsu@gmail.com>
2026-04-16 12:10:07 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -17,10 +17,9 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
-numero = int(input("Digite um número:"))
+numero = int(input())
 
 if numero % 2 == 0:
     print("Par")
-  
 else: 
     print("Ímpar")
